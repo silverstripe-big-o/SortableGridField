bc3732f1dec4629d78e0aa07cce2fe60267836ed
Ingo Schommer <ingo@silverstripe.com>
2012-08-21 21:31:41 +0200

    Fix JS indentation

--- javascript/GridFieldSortableRows.js
+++ javascript/GridFieldSortableRows.js
@@ -2,109 +2,104 @@
 	$.entwine('ss', function($) {
 		$('.ss-gridfield .gridfield-sortablerows input').entwine({
 			onmatch: function() {
-				var self=this;
-				var refCheckbox=$(this);
-				
-				var gridField=this.getGridField();
+				var self = this,
+					refCheckbox = $(this),
+					gridField = this.getGridField();
 				
 				if($(this).is(':checked')) {
 					gridField.find('table').addClass('dragSorting');
-				}else {
+				} else {
 					gridField.find('table').removeClass('dragSorting');
 				}
 				
 				gridField.find('tbody').sortable({
-												opacity: 0.6,
-												disabled: ($(this).is(':checked')==false),
-												helper: function(e, ui) {
-													//Maintains width of the columns
-													ui.children().each(function() {
-														$(this).width($(this).width());
-													});
-													
-													return ui;
-												},
-												update: function(event, ui) {
-													var dataRows=[];
-													var gridItems=gridField.getItems();
-													var button=refCheckbox.parent().find('.sortablerows-toggle');
-													
-													
-													for(var i=0;i<gridItems.length;i++) {
-														dataRows[i]=$(gridItems[i]).data('id');
-													}
-													
-													
-													self._makeRequest({data: [
-																				{
-																					name: button.attr('name'),
-																					value: button.val()
-																				},
-																				{
-																					name: 'Items',
-																					value: dataRows
-																				}
-																			]});
-												}
-											}).disableSelection();
+					opacity: 0.6,
+					disabled: ($(this).is(':checked')===false),
+					helper: function(e, ui) {
+						//Maintains width of the columns
+						ui.children().each(function() {
+							$(this).width($(this).width());
+						});
+						
+						return ui;
+					},
+					update: function(event, ui) {
+						var dataRows = [],
+							gridItems = gridField.getItems(),
+							button = refCheckbox.parent().find('.sortablerows-toggle');
+						
+						for(var i=0;i<gridItems.length;i++) {
+							dataRows[i] = $(gridItems[i]).data('id');
+						}
+						
+						self._makeRequest({data: [
+							{
+								name: button.attr('name'),
+								value: button.val()
+							},
+							{
+								name: 'Items',
+								value: dataRows
+							}
+						]});
+					}
+				}).disableSelection();
 				
 				gridField.find('.datagrid-pagination .ss-gridfield-previouspage, .datagrid-pagination .ss-gridfield-nextpage').each(function() {
-																$(this).droppable({
-																			disabled: $(this).is(':disabled'),
-																			accept: 'tr.ss-gridfield-item',
-																			activeClass: 'sortablerows-droptarget',
-																			tolerance: 'pointer',
-																			drop: function(event, ui) {
-																				gridField.find('tbody').sortable('cancel');
-																				
-																				var button=refCheckbox.parent().find('.sortablerows-sorttopage');
-																				var itemID=$(ui.draggable).data('id');
-																				var target='';
-																				
-																				
-																				if($(this).hasClass('ss-gridfield-previouspage')) {
-																					target='previouspage';
-																				}else if($(this).hasClass('ss-gridfield-nextpage')) {
-																					target='nextpage';
-																				}
-																				
-																				
-																				//Move and Reload the grid
-																				gridField.reload({data: [
-																											{
-																												name: button.attr('name'),
-																												value: button.val()
-																											},
-																											{
-																												name: 'ItemID',
-																												value: itemID
-																											},
-																											{
-																												name: 'Target',
-																												value: target
-																											}
-																										]});
-																				
-																				event.stopPropagation();
-																				event.stopImmediatePropagation();
-																			}
-																		});
-															});
+					$(this).droppable({
+						disabled: $(this).is(':disabled'),
+						accept: 'tr.ss-gridfield-item',
+						activeClass: 'sortablerows-droptarget',
+						tolerance: 'pointer',
+						drop: function(event, ui) {
+							gridField.find('tbody').sortable('cancel');
+							
+							var button = refCheckbox.parent().find('.sortablerows-sorttopage'),
+								itemID = $(ui.draggable).data('id'),
+								target = '';
+							
+							if($(this).hasClass('ss-gridfield-previouspage')) {
+								target = 'previouspage';
+							} else if($(this).hasClass('ss-gridfield-nextpage')) {
+								target = 'nextpage';
+							}
+							
+							//Move and Reload the grid
+							gridField.reload({data: [
+								{
+									name: button.attr('name'),
+									value: button.val()
+								},
+								{
+									name: 'ItemID',
+									value: itemID
+								},
+								{
+									name: 'Target',
+									value: target
+								}
+							]});
+							
+							event.stopPropagation();
+							event.stopImmediatePropagation();
+						}
+					});
+				});
 			},
 			
 			onchange: function(e) {
-				var gridField=this.getGridField();
-				gridField.find('tbody').sortable('option', 'disabled', ($(this).is(':checked')==false));
+				var gridField = this.getGridField();
+				gridField.find('tbody').sortable('option', 'disabled', ($(this).is(':checked')===false));
 				gridField.setState('GridFieldSortableRows', {sortableToggle: $(this).is(':checked')});
 				
 				
-				var button=$(this).parent().find('.sortablerows-disablepagenator');
+				var button = $(this).parent().find('.sortablerows-disablepagenator');
 				gridField.reload({data: [{name: button.attr('name'), value: button.val()}]});
 			},
 			
 			_makeRequest: function(ajaxOpts, callback) {
-				var gridField=this.getGridField();
-				var form = gridField.closest('form'), 
+				var gridField = this.getGridField(),
+					form = gridField.closest('form'), 
 					focusedElName = gridField.find(':input:focus').attr('name'); // Save focused element for restoring after refresh
 				
 				ajaxOpts.data = ajaxOpts.data.concat(form.find(':input').serializeArray());
